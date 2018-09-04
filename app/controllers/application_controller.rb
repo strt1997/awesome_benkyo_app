4f5ee37ad965c93c438b6d8b48c654133497211e
strt1997 <ayuhiko.angelo.wada@gmail.com>
2018-09-05 00:21:28 +0900

    Hello worldの部分を書き換えた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "Hello, world!"
   end
 
   def goodbye
     render html: "goodbye, world!"
   end
-  
+
 end
